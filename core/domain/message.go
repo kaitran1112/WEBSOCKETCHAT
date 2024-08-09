4a746e90b6591b6bd743bbb77fc39871e530bec6
tranthomanh <kaitran1112@gmail.com>
2024-08-09 14:11:08 +0700

    fix type of column Message

--- core/domain/message.go
+++ core/domain/message.go
@@ -4,8 +4,8 @@ import "time"
 
 type Message struct {
 	ID             uint      `json:"id" gorm:"primaryKey"`
-	SenderID       int       `json:"sender_id" gorm:"not null"`
-	ReceiverID     int       `json:"receiver_id" gorm:"not null"`
+	SenderID       uint      `json:"sender_id" gorm:"not null"`
+	ReceiverID     uint      `json:"receiver_id" gorm:"not null"`
 	MessageContent string    `json:"message_content" gorm:"type:text;not null"`
 	CreatedAt      time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
