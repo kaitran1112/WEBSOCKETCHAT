119095b5ff14b3da9be86460352c8b2b13b2a46a
tranthomanh <kaitran1112@gmail.com>
2024-08-09 14:23:33 +0700

    add define error response

---
+++ cmd/errors/enum.go
@@ -0,0 +1,10 @@
+package errors
+
+const (
+	ERROR_DATABASE_CODE           = 1
+	ERROR_DATABASE_MESSAGE        = "Error from database"
+	EXISTED_OBJECT_CODE           = 2
+	EXISTED_OBJECT_MESSAGE        = "Error existed object"
+	ERROR_CONTEXT_REQUEST_CODE    = 3
+	ERROR_CONTEXT_REQUEST_MESSAGE = "Error context request"
+)
